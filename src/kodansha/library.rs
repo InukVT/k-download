d56479dbbc5f257bb4f26597375ccc1cbf2ed85b
Bastian Inuk Christensen <jonalist@gmail.com>
2023-05-09 22:13:25 +0200

    removed unused types

--- src/kodansha/library.rs
+++ src/kodansha/library.rs
@@ -3,15 +3,6 @@ use serde::Deserialize;
 use crate::Volume;
 
 #[derive(Deserialize, Default, Clone)]
-pub struct Chapter;
-
-#[derive(Deserialize, Clone)]
-pub enum LibraryItem {
-    Chapter(Chapter),
-    Volume(Volume),
-}
-
-#[derive(Deserialize, Default, Clone)]
 pub struct Library {
     pub volumes: Vec<Volume>,
 }
